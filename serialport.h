60dda8327ee56e82097042ac44c16c1bb1b04a46
shihui <shihui.one@gmail.com>
2017-03-14 16:29:18 +0800

    add the head and source code for serial

---
+++ serialport.h
@@ -0,0 +1,21 @@
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+
+ /****************************************************************
+ * Constants
+ ****************************************************************/
+#define false 0
+#define true 1
+
+static int   speed_arr[] = { B115200, B19200, B9600, B4800, B2400, B1200, B300};
+static int   name_arr[] = {115200,  19200,  9600,  4800,  2400,  1200,  300};
+
+int SetDevice(int fd, int speed, int flow_ctrl, int databits, int stopbits, int parity);
+int OpenDevice(const char* port);
+void Close_Dev(int fd);
+int InitDevice(int fd, int speed, int flow_ctrl, int databits, int stopbits, int parity);
+int Receive(int fd, char *rcv_buf,int data_len);
+int Send(int fd, char *send_buf,int data_len);
+
+
+#endif // SERIALPORT_H_
